2fe13c18ac5ecc3ae5d5166af4c059c8afd64081
xufeixiang <xufeixiang@ainanjing.org.cn>
2024-09-10 16:00:48 +0800

    chore: remove sqlite database.

--- files/sdk_python/settings.py
+++ files/sdk_python/settings.py
@@ -71,12 +71,7 @@ WSGI_APPLICATION = 'sdk_python.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
